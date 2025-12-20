540884fa50d7cf053fecb6f80ed34bc5aa7a84e2
Federico Bojaminsky <federico.bojaminsky@openit.com.ar>
2025-12-20 11:33:10 -0300

    update version

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from 'next-auth'
 import GoogleProvider from 'next-auth/providers/google'
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     // OAuth authentication providers...
 
@@ -10,4 +10,6 @@ export default NextAuth({
       clientSecret: process.env.GOOGLE_SECRET
     }),
   ]
-})
+};
+
+export default NextAuth(authOptions);
