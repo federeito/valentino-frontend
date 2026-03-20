823b5c621c597b9d6359244b0ea05b0ede83e5a4
Federico Bojaminsky <federico.bojaminsky@openit.com.ar>
2026-03-20 18:50:45 -0300

    sitemapfix

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,7 +3,7 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://www.valentinoaccesorios.com.ar',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
-  exclude: ['/api/*', '/admin/*', '/products/*'],
+  exclude: ['/api/*', '/admin/*', '/products', '/products/*'],
   robotsTxtOptions: {
     policies: [
       {
